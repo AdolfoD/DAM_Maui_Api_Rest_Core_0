fdf76cc3dd22dd16d9eefd91e263e900448f0f24
Adolfo De Sixto Quintos <adolfodesixto17@gmail.com>
2025-06-11 11:34:24 -0600

    Version 01: Capa Api, Capa Entidad, Prueba de Dependencia

---
+++ CapaEntidad/PersonaCLS.cs
@@ -0,0 +1,12 @@
+﻿namespace CapaEntidad
+{
+    public class PersonaClS
+    {
+        //Listado
+
+        public int iidpersona { get; set; }
+        public string nombrecompleto { get; set; }
+        public string correo { get; set; }
+        public string fechanacimientocadena { get; set; }
+    }
+}
---
+++ Web.Api/Controllers/PersonaController.cs
@@ -0,0 +1,45 @@
+﻿using CapaEntidad;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web.Api.Models;
+
+
+namespace Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        [HttpGet("{nombrecompleto}")]
+
+        public List<PersonaClS> listarPersona(string nombrecompleto)
+        {
+            List<PersonaClS> lista = new List<PersonaClS>();
+
+            try
+            {
+                using (DbAba245BdveterinariaContext bd = new DbAba245BdveterinariaContext())
+                {
+                    lista = (from persona in bd.Personas
+                             where persona.Bhabilitado == 1
+                             && (persona.Nombre + " " + persona.Appaterno + " " + persona.Apmaterno).Contains(nombrecompleto)
+                             select new PersonaClS
+                             {
+                                 iidpersona = persona.Iidpersona,
+                                 nombrecompleto = persona.Nombre + " " + persona.Appaterno + " " + persona.Apmaterno,
+                                 correo = persona.Correo,
+                                 fechanacimientocadena = persona.Fechanacimiento == null ? "" :
+                                 persona.Fechanacimiento.Value.ToShortDateString()
+
+                             }
+                         ).ToList();
+                }
+                return lista;
+            }
+            catch(Exception ex)
+            {
+                return lista;
+            }
+        }
+    }
+}
